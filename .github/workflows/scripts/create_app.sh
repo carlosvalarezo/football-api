a68a81adb49ed55a84603b97e3a09ea23d34ccb0
Carlos Valarezo <carlos.valarezo@gmail.com>
2022-09-09 13:00:56 -0500

    Comments create_app script
    The GH action create_app workflow should be executed only once. The script should also include a validation of the apps from heroku.

--- .github/workflows/scripts/create_app.sh
+++ .github/workflows/scripts/create_app.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-git config --global user.email "${GITHUB_EMAIL}"
-git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "$GITHUB_EMAIL"
+git config --global user.name "$GITHUB_ACTOR"
+# shellcheck disable=SC1073
 cat << EOF > ~/.netrc
   machine cv-football-api
     login "$HEROKU_USERNAME"
     password "$HEROKU_API_KEY"
-  EOF
+  # shellcheck disable=SC1039
+EOF
 heroku create -a cv-football-api
